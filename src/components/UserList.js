9216767cafd5159648ebb5bb3c3a17cd97d7d8fc
Mahesh M Nimkar <m.nimkar070@gmail.com>
2024-04-20 18:34:54 +0530

    Add , Edit, Delete functionality

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,18 +3,30 @@ import axios from "axios";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [lastName,setLastName] = useState("")
+  const [email,setEmail] = useState("");
+  const [phone,setPhone] = useState("");
+  const [updateName, setUpdateName] = useState("");
+  const [updateEmail,setUpdateEmail] = useState("");
+  const [updatePhone,setUpdatePhone] = useState("");
+  const [editId,setEditID] = useState(-1);
+
 
   useEffect(() => {
+    //Fetching a data from api
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
         setUsers(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.error("Error fetching users:", error);
       });
   }, []);
 
+  //Function to delete a User .
   const deleteUser = (userId) => {
     axios
       .delete(`https://jsonplaceholder.typicode.com/users/${userId}`)
@@ -28,16 +40,121 @@ const UserList = () => {
       });
   };
 
+  
+
+  //Function Of Api call to add data in database
+  const postCall=async()=>{
+    await axios.post(`https://jsonplaceholder.typicode.com/users/`,{id:users[users.length-1].id+1,name:name,email:email,phone:phone})
+    .then(res=>alert("user created successfully"))
+    .catch((e)=>{
+      alert("There some issue while Creating new User")
+    })
+  }
+
+  //Function to Add new User in UserList
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postCall();
+    setUsers((prev)=>[...prev,{
+      id:users[users.length-1].id+1,name:name,email:email,phone:phone
+    }])
+    setName("");
+    setEmail("");
+    setPhone("");
+  };
+
+  //Function to Handle edit input values
+  const handleEdit = (id)=>{
+    const filter_single_data = users.filter(item=>item.id === id);
+    const fullname = filter_single_data[0].name.split(" ");
+
+    setUpdateName(fullname[0]);
+    setLastName(fullname[1]);
+    setUpdateEmail(filter_single_data[0].email);
+    setUpdatePhone(filter_single_data[0].phone);
+
+    setEditID(id);
+  }
+  console.log(editId);
+
+
+  //Function to handle the Update functionality
+  const handleUpdate = (id)=>{
+    let fullname =updateName+" "+lastName;
+    axios.put(`https://jsonplaceholder.typicode.com/users/${editId}`, {id:editId,name:fullname,email:updateEmail,phone:updatePhone})
+    .then(alert("Added new user successfully"))
+    .then(window.location.reload())
+    .catch((e)=>{
+      alert("Error while adding a User");
+    })
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === id
+          ? { ...user, name: updateName, email: updateEmail , phone: phone }
+          : user
+      )
+    );
+    
+  }
+
   return (
     <>
       <div className="p-5 bg-gray-100">
-        <div className="flex items-center justify-between">
-          <span className="text-xl mb-2 text-left font-bold">
+        <div className="flex items-center justify-center">
+          <h1 className="text-xl mb-2 text-left font-bold">
             UserList Dashboard
-          </span>
-          <span className="text-xl mb-2 text-end font-bold">Add</span>
+          </h1>
         </div>
 
+
+        {/* Add Functionality Code JSX */}
+        <div className="border p-1 bg-slate-50 rounded-lg my-1">
+          <form
+            onSubmit={handleSubmit}
+            className="flex items-center justify-between"
+          >
+            <div>
+              <input
+                type="text"
+                required
+                className="border rounded-md m-0.5"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+              />
+              <input
+                type="text"
+                required
+                className="border rounded-md m-0.5"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+              />
+              <input
+                type="text"
+                required
+                className="border rounded-md m-0.5"
+                placeholder="Company Name"
+                name="cName"
+                value={phone}
+                onChange={(e)=>setPhone(e.target.value)}
+              />
+            </div>
+            <button alt="add">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="cursor-pointer border rounded-md w-7"
+              >
+                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+              </svg>
+            </button>
+          </form>
+        </div>
+
+        {/* Desktop view Code */}
         <div className="overflow-auto rounded-lg shadow hidden md:block bg-white">
           <table className="w-full">
             <thead className="bg-grey-200 border-b-2 border-grey-200">
@@ -55,7 +172,7 @@ const UserList = () => {
                   Email
                 </th>
                 <th className="p-3 text-sm font-semibold tracking-wide text-left whitespace-nowrap">
-                  Company Name
+                  Phone
                 </th>
                 <th className="p-3 text-sm font-semibold tracking-wide text-left whitespace-nowrap">
                   Actions
@@ -63,7 +180,40 @@ const UserList = () => {
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-200">
+
+              {/* Looping through the array of objects to gererate table data*/}
+
               {users.map((user) => (
+                user.id ===editId ? 
+
+                // Conditionally rendering for editing the records
+
+                <tr key={user.id}>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {user.id}
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    <input type="text" value={updateName} onChange={(e)=>setUpdateName(e.target.value)} />
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    <input type="text" value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    <input type="text" value={updateEmail} onChange={(e)=>setUpdateEmail(e.target.value)}/>
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    <input type="text" value={updatePhone} onChange={(e)=>setUpdatePhone(e.target.value)}/>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <button
+                      className="text-indigo-600 hover:text-white border px-1 rounded-lg bg-green-300 mr-1"
+                      onClick={() => handleUpdate(user.id)}
+                    >
+                      Update
+                    </button>
+                  </td>
+                </tr>
+                :
                 <tr key={user.id}>
                   <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
                     {user.id}
@@ -74,16 +224,16 @@ const UserList = () => {
                   <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
                     {user.name.split(" ")[1]}
                   </td>
-                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <td className="p-3 text-sm text-gray-500 whitespace-nowrap">
                     {user.email}
                   </td>
-                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                    {user.company.name}
+                  <td className="p-3 text-sm text-gray-500 whitespace-nowrap">
+                    {user.phone}
                   </td>
                   <td className="px-6 py-4 whitespace-nowrap">
                     <button
                       className="text-indigo-600 hover:text-white border px-1 rounded-lg bg-green-300 mr-1"
-                      onClick={() => console.log(user.id)}
+                      onClick={() => handleEdit(user.id)}
                     >
                       Edit
                     </button>
@@ -100,8 +250,34 @@ const UserList = () => {
           </table>
         </div>
 
+              {/* Mobile view Code */}
+
         <div className="grid grid-cols-1  sm:grid-cols-2 gap-4 md:hidden">
           {users.map((user) => (
+            user.id ===editId ?
+            <div className="bg-white overflow-auto p-4 rounded-lg space-y-3 roun-lg shadow"
+            key={user.id}>
+              <div className="flex items-center space-x-2 text-sm whitespace-nowrap">
+                <div>ID : {user.id}</div>
+              </div>
+              <div className="flex items-center space-x-2 text-sm whitespace-nowrap">
+                <div>{user.name.split(" ")[0]}</div>
+                <div>{user.name.split(" ")[1]}</div>
+              </div>
+              <div className="flex items-center space-x-2 text-sm whitespace-nowrap">
+                <div className="text-gray-500">{user.email}</div>
+                <div className="text-gray-500"> {user.phone}</div>
+              </div>
+              <div className="flex items-center space-x-2 text-sm">
+                <button
+                  className="text-indigo-600 hover:text-white border px-1 rounded-lg bg-green-300 mr-1"
+                  onClick={() =>handleUpdate(user.id)}
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+            :
             <div
               className="bg-white overflow-auto p-4 rounded-lg space-y-3 roun-lg shadow"
               key={user.id}
@@ -114,19 +290,19 @@ const UserList = () => {
                 <div>{user.name.split(" ")[1]}</div>
               </div>
               <div className="flex items-center space-x-2 text-sm whitespace-nowrap">
-                <div className="text-gray-300">{user.email}</div>
-                <div className="text-gray-300"> {user.company.name}</div>
+                <div className="text-gray-500">{user.email}</div>
+                <div className="text-gray-500"> {user.phone}</div>
               </div>
               <div className="flex items-center space-x-2 text-sm">
                 <button
                   className="text-indigo-600 hover:text-white border px-1 rounded-lg bg-green-300 mr-1"
-                  onClick={() => console.log("user.id")}
+                  onClick={() =>handleEdit(user.id)}
                 >
                   Edit
                 </button>
                 <button
                   className="text-indigo-600 hover:text-white border px-1 rounded-lg bg-red-300 ml-1"
-                  onClick={() => deleteUser("user.id")}
+                  onClick={() => deleteUser(user.id)}
                 >
                   Delete
                 </button>
